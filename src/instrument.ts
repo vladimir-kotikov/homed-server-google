fed5a8464d16169b5b31cb90fdc6bc987723f59e
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2026-02-03 14:11:27 +0200

    Debug Sentry instrumentation

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,8 +1,28 @@
 import * as Sentry from "@sentry/node";
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  environment: process.env.NODE_ENV,
-  enableLogs: true,
-  tracesSampleRate: 1.0,
-});
+const sentry_environment = Object.keys(process.env).filter(key =>
+  key.startsWith("SENTRY_")
+);
+
+if (sentry_environment.length > 0) {
+  console.log(`Sentry environment variables: ${sentry_environment.join(", ")}`);
+}
+
+if (process.env.SENTRY_DSN !== "") {
+  console.log(
+    `Initializing Sentry with DSN: ${process.env.SENTRY_DSN} \
+    environment: ${process.env.NODE_ENV}`
+  );
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+    enableLogs: true,
+    tracesSampleRate: 1.0,
+    integrations: [
+      Sentry.expressIntegration(),
+      Sentry.zodErrorsIntegration(),
+      Sentry.consoleIntegration(),
+      Sentry.httpServerIntegration(),
+    ],
+  });
+}
